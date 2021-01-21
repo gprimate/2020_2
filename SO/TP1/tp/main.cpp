3990a6c1ce7f5b7dcfbdf68659d552bd35935a07
Guilherme Pereira <gprimate@gmail.com>
2021-01-21 17:23:12 -0300

    Pronto - sem print "voltou ao trabalho"

--- SO/TP1/tp/main.cpp
+++ SO/TP1/tp/main.cpp
@@ -4,28 +4,30 @@
 int main(int argc, char const *argv[]) {
     
     int numCharacters = 8;
-    int numIterations = 2;
+    int numIterations = 5;
 
     pthread_t cenouras[numCharacters];
+    pthread_t raj;
 
     int ids[numCharacters];
-    ThreadInfo info[numCharacters];
+    ThreadArg info[numCharacters];
 
     for (int i = 0; i < numCharacters; i++) {
         info[i].id = i;
-        info[i].numIter = numIterations;
+        info[i].num_iteracoes = numIterations;
 
     }
 
     for (int i = 0; i < numCharacters; i++) {
-        pthread_create(&(cenouras[i]), NULL, bananinha, &info[i]);
+        pthread_create(&(cenouras[i]), NULL, inicia_casais, &info[i]);
     }
+    pthread_create(&raj, NULL, inicia_raj, NULL);
 
     for(int i = 0; i < numCharacters; i++) {
         pthread_join(cenouras[i], NULL);
     }
+    pthread_join(raj, NULL);
     
-    
-    
+
     return 0;
 }
